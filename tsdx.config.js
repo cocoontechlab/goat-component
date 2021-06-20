9fb9815cd6c1e1f96003b6d47710d99e19954bf6
unknown <pracharya2601@gmail.com>
2021-06-19 20:53:09 -0700

    fix: postcss

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,17 +1,10 @@
 const postcss = require('rollup-plugin-postcss');
+
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
-        modules: true,
-        config: {
-          path: './postcss.config.js',
-        },
-        extensions: ['.css'],
-        minimize: true,
-        inject: {
-          insertAt: 'top',
-        },
+        plugins: [],
       })
     );
     return config;
