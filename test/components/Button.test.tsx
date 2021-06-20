a0a2ef72ea60361ba93f605c83b70a7496accd75
unknown <pracharya2601@gmail.com>
2021-06-20 15:49:33 -0700

    fix: button test change

--- test/components/Button.test.tsx
+++ test/components/Button.test.tsx
@@ -29,23 +29,4 @@ describe('Button', () => {
     // check that the function was called once;
     expect(mockClick).toHaveBeenCalledTimes(1);
   });
-
-  it('should have the right text color class name for variants', () => {
-    // we extract the "rerender" method to test both variants
-    const { getByRole, rerender } = render(<Button text="test" />);
-
-    const buttonElement = getByRole('button', { name: 'test' });
-
-    // if you recall, passing no variant, defaults to "default" variant.
-    // this is a bit robust, but it serves to illustarte the point
-    expect(buttonElement.classList.contains('text-white')).toBe(true);
-    expect(buttonElement.classList.contains('text-red-700')).toBe(false);
-
-    // render the other "warning" variant
-    rerender(<Button text={'test'} variant="warning" />);
-
-    // test the opposite of the above:
-    expect(buttonElement.classList.contains('text-white')).toBe(false);
-    expect(buttonElement.classList.contains('text-red-700')).toBe(true);
-  });
 });
