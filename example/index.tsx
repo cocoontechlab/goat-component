7d489acb8747827624d81c6500d6d143afebbc34
unknown <pracharya2601@gmail.com>
2021-06-19 20:24:11 -0700

    fix(postcss): postcss

---
+++ example/index.tsx
@@ -0,0 +1,17 @@
+
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Thing } from '../.';
+import { Button } from '../.';
+
+const App = () => {
+  return (
+    <div>
+      <Thing />
+      <Button text="Button" />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
